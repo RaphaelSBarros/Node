3af94fd4d74f1ba1f9eaf7076162d16c44193593
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-06-01 15:25:58 -0300

    Instalando o Express, criando um servidor e exibindo uma mensagem de Olá Mundo

--- intro/src/index.ts
+++
@@ -1,9 +0,0 @@
-import validator from 'validator';
-
-let nome: string = 'Raphael';
-
-if(validator.isLowercase(nome)){
-    console.log(`A string ${nome} é toda minúscula`);
-}else{
-    console.log(`A string ${nome} NÃO é toda minúscula`);
-}
