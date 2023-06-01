c605aef7b973dc4b8e3099e81130adf647e30270
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-06-01 16:46:11 -0300

    Organizando as rotas em arquivos separados

--- intro/src/server.ts
+++ intro/src/server.ts
@@ -1,21 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import mainRoutes from './routes/index'
+import painelRoutes from './routes/painel'
 
 const server = express();
 
-server.get('/', (req: Request, res: Response)=>{
-    res.send('Olá Mundo!');
-});
-
-server.get('/noticia/:slug', (req: Request, res: Response)=>{
-    let slug: string = req.params.slug;
-    res.send(`Notícia: ${slug}`);
-});
-
-server.get('/voo/:origem-:destino', (req: Request, res: Response)=>{
-
-    let { origem, destino } = req.params;
-
-    res.send(`Procurando voos de ${origem.toUpperCase()} até ${destino.toUpperCase()}`);
-});
+server.use(mainRoutes);
+server.use('/painel', painelRoutes);
 
 server.listen(80);
