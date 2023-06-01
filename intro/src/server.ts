e8fdd6bc403cd6369a2257bc2e752ca34242200e
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-06-01 16:14:53 -0300

    Entendendo rotas e iniciando na criação de rotas dinâmicas

--- intro/src/server.ts
+++ intro/src/server.ts
@@ -6,4 +6,16 @@ server.get('/', (req: Request, res: Response)=>{
     res.send('Olá Mundo!');
 });
 
+server.get('/noticia/:slug', (req: Request, res: Response)=>{
+    let slug: string = req.params.slug;
+    res.send(`Notícia: ${slug}`);
+});
+
+server.get('/voo/:origem-:destino', (req: Request, res: Response)=>{
+
+    let { origem, destino } = req.params;
+
+    res.send(`Procurando voos de ${origem.toUpperCase()} até ${destino.toUpperCase()}`);
+});
+
 server.listen(80);
