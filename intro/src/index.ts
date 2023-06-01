caa89e983e53a3d3f8721515518b07ae4dc99c13
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-06-01 14:48:19 -0300

    Importando e utilizando arquivos de terceiros

--- intro/src/index.ts
+++ intro/src/index.ts
@@ -1,10 +1,5 @@
-import * as Matematica from './Matematica'; //(importando tudo que tem export no arquivo)
-//import { somar, subtrair, multiplicar } from './Matematica'; (importando funções específicas com export no arquivo)
-//import Matematica from './Matematica'; (importa tudo que exportado como default)
+import validator from 'validator';
 
-let n1: number = 10;
-let n2: number = 2;
+let ip = '127.999.0.1';
 
-console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
-console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
-console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
+console.log(validator.isIP(ip));
