c4766d8ed3edc39d51980c9ad4730ccfe04d383c
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-06-03 16:44:57 -0300

    Recebendo os dados de forma discreta com o método POST

--- intro/src/routes/index.ts
+++ intro/src/routes/index.ts
@@ -43,13 +43,18 @@ router.get('/nome', (req: Request, res: Response) => {
         nome,
         idade
     });
-})
+});
+
 router.get('/idade', (req: Request, res: Response) => {
+    res.render('pages/idade');
+});
+
+router.post('/idade-resultado', (req: Request, res: Response)=>{
     let mostrarIdade: boolean = false;
     let idade: number = 0;
 
-    if(req.query.ano){
-        let anoNascimento: number = parseInt(req.query.ano as string);
+    if(req.body.ano){
+        let anoNascimento: number = parseInt(req.body.ano as string);
         let anoAtual: number = new Date().getFullYear();
         idade = anoAtual - anoNascimento;
         mostrarIdade = true;
@@ -57,7 +62,7 @@ router.get('/idade', (req: Request, res: Response) => {
     res.render('pages/idade', {
         idade,
         mostrarIdade
-    })
-});
+    });
+})
 
 export default router;
